a2e2757ca66345cb5854096ed5219731555dc1e8
danielalvarado <danialvarado@ic-itcr.ac.cr>
2018-02-17 22:17:44 -0600

    More updates through page 76

--- mean-todo-app/config/express.js
+++ mean-todo-app/config/express.js
@@ -1,8 +1,10 @@
-var express = require('express'),
-     morgan = require('morgan'),
-     compress = require('compression'),
-     bodyParser = require('body-parser'),
-     methodOverride = require('method-override');
+var config = require('./config'),
+express = require('express'),
+morgan = require('morgan'),
+compress = require('compression'), 
+bodyParser = require('body-parser'), 
+methodOverride = require('method-override'), 
+session = require('express-session');
    module.exports = function() {
      var app = express();
      if (process.env.NODE_ENV === 'development') {
@@ -15,8 +17,16 @@ var express = require('express'),
      }));
      app.use(bodyParser.json());
      app.use(methodOverride());
+     app.use(session({
+       saveUninitialized: true,
+       resave: true,
+       secret: config.sessionSecret
+}));
+     app.set('views', './app/views');
+     app.set('view engine', 'ejs');
      require('../app/routes/index.server.routes.js')(app);
-  return app;
+     app.use(express.static('./public'));
+   return app;
 };
 
 
